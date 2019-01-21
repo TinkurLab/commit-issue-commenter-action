c8b3276c01121cb74724ce418db10bd14a6824a7
Adam Zolyak <adam.zolyak@gmail.com>
2019-01-21 12:38:05 -0700

    wip

--- Dockerfile
+++ Dockerfile
@@ -5,9 +5,9 @@ LABEL "com.github.actions.description"="Comment on issues from commit messages"
 LABEL "com.github.actions.icon"="mic"
 LABEL "com.github.actions.color"="green"
 
-LABEL "repository"="http://github.com/waffleio/gh-actions"
-LABEL "homepage"="http://www.waffle.io"
-LABEL "maintainer"="Adam Zolyak <adam@waffle.com>"
+LABEL "repository"="http://github.com/adamzolyak/commit-issue-commenter-action"
+LABEL "homepage"="http://www.tinkurlab.com"
+LABEL "maintainer"="Adam Zolyak <adam@tinkurlab.com>"
 
 ADD entrypoint.sh /action/entrypoint.sh
 ADD package.json /action/package.json
--- tests/helpers.test.js
+++ tests/helpers.test.js
@@ -5,58 +5,66 @@ octokit = jest.fn()
 octokit.authenticate = jest.fn()
 
 describe('getOwner', () => {
-    it('should return owner when passed GITHUB_REPOSITORY env variable', () => {
-        const result = helpers.getOwner('waffleio/waffle.io')
-        expect(result).toBe('waffleio')
-    })
+  it('should return owner when passed GITHUB_REPOSITORY env variable', () => {
+    const result = helpers.getOwner('adamzolyak/actions-playground')
+    expect(result).toBe('adamzolyak')
+  })
 })
 
 describe('getRepo', () => {
-    it('should return repo when passed GITHUB_REPOSITORY env variable', () => {
-        const result = helpers.getRepo('waffleio/waffle.io')
-        expect(result).toBe('waffle.io')
-    })
+  it('should return repo when passed GITHUB_REPOSITORY env variable', () => {
+    const result = helpers.getRepo('adamzolyak/actions-playground')
+    expect(result).toBe('actions-playground')
+  })
 })
 
 describe('getIssueFromBranch', () => {
-    it('should return false if branch does NOT start with issue number', async () => {  
-        const ref = 'refs/heads/mybranch'
-        const result = helpers.getIssueFromBranch(ref)
-        expect(result).toBe(false)
-    })
+  it('should return false if branch does NOT start with issue number', async () => {
+    const ref = 'refs/heads/mybranch'
+    const result = helpers.getIssueFromBranch(ref)
+    expect(result).toBe(false)
+  })
 
-    it('should return issue number if branch does start with issue number', async () => {  
-        const ref = 'refs/heads/22-mybranch'
-        const result = helpers.getIssueFromBranch(ref)
-        expect(result).toBe('22')
-    })
+  it('should return issue number if branch does start with issue number', async () => {
+    const ref = 'refs/heads/22-mybranch'
+    const result = helpers.getIssueFromBranch(ref)
+    expect(result).toBe('22')
+  })
 })
 
 describe('checkForCommitActions', () => {
-    it('should return false if commit does NOT include a comment', async () => {  
-        const commitMessage = 'added initial ui'
-        const result = helpers.checkForCommitActions(commitMessage)
-        expect(result).toBe(false)
-    })
+  it('should return false if commit does NOT include a comment', async () => {
+    const commitMessage = 'added initial ui'
+    const result = helpers.checkForCommitActions(commitMessage)
+    expect(result).toBe(false)
+  })
 
-    it('should return false if commit does NOT include a comment', async () => {  
-        const commitMessage = 'added initial ui #comment fyi @adamzolyak you might want to check this out'
-        const result = helpers.checkForCommitActions(commitMessage)
-        expect(result).toBe('fyi @adamzolyak you might want to check this out')
-    })
+  it('should return false if commit does NOT include a comment', async () => {
+    const commitMessage =
+      'added initial ui #comment fyi @adamzolyak you might want to check this out'
+    const result = helpers.checkForCommitActions(commitMessage)
+    expect(result).toBe('fyi @adamzolyak you might want to check this out')
+  })
 })
 
 describe('addComment', () => {
-    it('should reopen the specified issue', async () => {  
-        
-        let octokit = {
-            issues: {
-                createComment: jest.fn().mockResolvedValue( { something: 'something' })
-            }
-        }
+  it('should reopen the specified issue', async () => {
+    let octokit = {
+      issues: {
+        createComment: jest.fn().mockResolvedValue({ something: 'something' })
+      }
+    }
 
-        const result = await helpers.addComment(octokit, 'waffleio', 'waffle.io', '1', 'this is a comment')
-        expect(octokit.issues.createComment).toHaveBeenCalledTimes(1)
-        expect(octokit.issues.createComment.mock.calls[0][0].body).toMatch(/this is a comment/)
-    })
+    const result = await helpers.addComment(
+      octokit,
+      'adamzolyak',
+      'actions-playground',
+      '1',
+      'this is a comment'
+    )
+    expect(octokit.issues.createComment).toHaveBeenCalledTimes(1)
+    expect(octokit.issues.createComment.mock.calls[0][0].body).toMatch(
+      /this is a comment/
+    )
+  })
 })
